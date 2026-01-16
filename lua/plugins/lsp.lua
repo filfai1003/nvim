b0b584b581d74af57464eb360a5e0aac4ff750cb
filfai1003 <filfai1003@gmail.com>
2026-01-16 22:58:55 +0100

    Refactor LSP configuration, remove unused Mason plugin, and create new DAP plugin file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,13 @@
-
 return {
-  "neovim/nvim-lspconfig",
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = {            
+            -- Java LSP
+            "jdtls",
+        },
+    },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
 }
-
