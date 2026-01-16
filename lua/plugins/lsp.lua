add618b290f58764cef8a7f90ad79a91e0d46ad4
filfai1003 <filfai1003@gmail.com>
2026-01-16 16:31:23 +0100

    Update LSP configuration to include 'build.gradle' in root directory search

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
             cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
             filetypes = { "java" },
             root_dir = vim.fs.dirname(
-                vim.fs.find({ "pom.xml", ".git" }, { upward = true })[1]
+                vim.fs.find({ "pom.xml", "build.gradle", ".git" }, { upward = true })[1]
             ),
         })
         vim.lsp.enable("jdtls")
